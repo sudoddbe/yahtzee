3285f9ac77dfa1325164dd601abe6be7c6e8d696
David Belfrage <david.belfrage@hotmail.com>
2018-11-15 19:30:01 +0100

    * Renamed yahtzee, added scoring functions

--- yahtzee_scorecard.py
+++ yahtzee_scorecard.py
@@ -1,4 +1,5 @@
 from yahtzee_probability import dice_probability_dict 
+import numpy as np
 import itertools
 
 Class Scorecard:
@@ -20,15 +21,15 @@ Class Scorecard:
             'yahtzee'
         ]
         scoring_functions = [
-            self.score_ones(face = 1),
-            self.score_twos(face = 2),
-            self.score_threes(face = 3),
-            self.score_fours(face = 4),
-            self.score_fives(face = 5),
-            self.score_sixes(face = 6),
-            self.score_pair(n = 2),
-            self.score_three_of_a_kind(n = 3),
-            self.score_four_of_a_kind((n = 4)),
+            self.score_face(face = 1),
+            self.score_face(face = 2),
+            self.score_face(face = 3),
+            self.score_face(face = 4),
+            self.score_face(face = 5),
+            self.score_face(face = 6),
+            self.score_n_of_a_kind(n = 2),
+            self.score_n_of_a_kind(n = 3),
+            self.score_n_of_a_kind((n = 4)),
             self.score_full_house(),
             self.score_small_straight(),
             self.score_large_straight(),
@@ -37,44 +38,44 @@ Class Scorecard:
         self.categories = [Category(label, scoring_function) for label, scoring_function in zip(labels, scoring_functions)]
     def score_face(self, rolls, face):
         score = 0
-        for roll in rolls:
-            if roll == face:
-                score +=face
-        return score
+        bins = np.binscount(rolls)
+        if len(bins) < face:
+            return 0
+        else:
+            return bins[face] * face
 
     def score_n_of_a_kind(self, rolls, n):
         last_roll = 7
         rolls_in_a_row = 0
-        for roll in rolls[::-1]:
-            assert last_roll >= roll
-            if roll == last_roll:
-                #highest n-of-a-kind found (assumes sorted rolls)
-                rolls_in_a_row += 1
-                if rolls_in_a_row == n:
-                    return n*roll
-            else:
-                last_roll = roll
-                rolls_in_a_row = 0
+        bins = np.binscount(rolls)
+        last_roll = len(bins)
+        for bin in bins[::-1]:
+            if bin >= n:
+                return last_roll*n
+            last_roll -= 1
         return 0
 
-    #NOT FINISHED
     def score_full_house(self, rolls):
-        last_roll = 7
-        rolls_in_a_row = 0
-        three_of_a_kind_score = 0
-        pair_score = 0
-        for roll in rolls[::-1]:
-            assert last_roll >= roll
-            if roll == last_roll:
-                rolls_in_a_row += 1
-                if rolls_in_a_row == 3:
-                    three_of_a_kind_score 3*roll
-            else:
-                if rolls_in_a_row == 2:
-                    pair_score = 2*rolls
-                    if three_of_kind_score
-                last_roll = roll
-                rolls_in_a_row = 0
+        bins = np.bincount(rolls)
+        if len(bins > 0) == 2:
+            assert(len(bins == 2) == 1 and len(bins ==3) == 1)
+            return np.inner(bins, np.arange(len(bins)))
+        return 0
+
+    def score_small_straight(self, rolls):
+        if rolls == (1,2,3,4,5):
+            return 1 + 2+ 3+ 4+ 5
+        return 0 
+
+    def score_large_straight(self, rolls):
+        if rolls == (2,3,4,5,6):
+            return  2+ 3+ 4+ 5 +6
+        return 0 
+
+    def score_yahtzee(self, rolls):
+        bins = np.binscount(rolls)
+        if np.max(bins) == 5:
+            return 50
         return 0
 
 class Category():
@@ -93,8 +94,8 @@ class Category():
 
 if __name__ == "__main__":
     dice_values = [1,2,3,4,5,6]
-    input_sets = [p for n in range(7) for p in itertools.combinations_with_replacement(dice_values, n)]
-    output_sets = [p for p in itertools.combinations_with_replacement(dice_values, 6)]
+    input_sets = [p for n in range(6) for p in itertools.combinations_with_replacement(dice_values, n)]
+    output_sets = [p for p in itertools.combinations_with_replacement(dice_values, 5)]
     print len(output_sets)
     print len(input_sets)
     fpd, rpd = dice_probability_dict(input_sets, output_sets)
