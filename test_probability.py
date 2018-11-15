a8d1ad4ded383edaada2d64ccd9f99e18f50eba3
David Belfrage <david.belfrage@hotmail.com>
2018-11-15 19:25:23 +0100

    * Changed to five dices in test

--- test_probability.py
+++ test_probability.py
@@ -11,18 +11,21 @@ def test_forward_and_backwards_probabilities(fpd, rpd):
 #check results with pen and paper calculations...
 #Might be redundant as this understanding is the input to the probability 
 #calculations
-def test_explicit_probability(fpd, input_sets, output_sets, expected_probabilities):
+def test_explicit_probability(fpd, input_sets, output_sets, expected_probabilities, verbose = False):
     for input_set, output_set, probability in zip(input_sets, output_sets, expected_probabilities):
+            if verbose:
+                print str(input_set) + " : " + str(output_set) + " - " + str(probability) + " : " + str(fpd[input_set][output_set])
             assert fpd[input_set][output_set] == probability
 
 if __name__ == "__main__":
     dice_values = [1,2,3,4,5,6]
-    input_sets = [p for n in range(7) for p in itertools.combinations_with_replacement(dice_values, n)]
-    output_sets = [p for p in itertools.combinations_with_replacement(dice_values, 6)]
+    input_sets = [p for n in range(6) for p in itertools.combinations_with_replacement(dice_values, n)]
+    output_sets = [p for p in itertools.combinations_with_replacement(dice_values, 5)]
     fpd, rpd = dice_probability_dict(input_sets, output_sets)
     test_forward_and_backwards_probabilities(fpd,rpd)
     
-    input_sets = [(), (1, 2, 3, 4, 5), (1, 2, 3, 4), (1, 2, 3, 4), (1, 1, 1)]
-    output_sets = [(6,6,6,6,6,6), (1, 2, 3, 4, 5, 6), (1,2,3,4,5,6), (1,2,3,4,6,6), (1, 2, 3, 4, 5, 6)]
-    expected_probabilities = [(1/6.0)**6, (1/6.0), 2*(1/6.0)**2, (1/6.0)**2, 0.0]
-    test_explicit_probability(fpd, input_sets, output_sets, expected_probabilities)
+    input_sets = [(), (1, 2, 3, 4), (1, 2, 3), (1, 2, 3), (1, 1, 1)]
+    output_sets = [(6,6,6,6,6), (1, 2, 3, 4, 5), (1,2,3,4,5), (1,2,3,6,6), (1, 2, 3, 4, 5)]
+    expected_probabilities = [(1/6.0)**5, (1/6.0), 2*(1/6.0)**2, (1/6.0)**2, 0.0]
+    verbose = True
+    test_explicit_probability(fpd, input_sets, output_sets, expected_probabilities, verbose = verbose)
