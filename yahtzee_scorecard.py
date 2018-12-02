92feaf0263b4b6de7b1c22d8035e95bcebe11b76
David Belfrage <david.belfrage@hotmail.com>
2018-12-02 10:45:13 +0100

    * Changed scoring functions to use generic

--- yahtzee_scorecard.py
+++ yahtzee_scorecard.py
@@ -1,8 +1,9 @@
 from yahtzee_probability import dice_probability_dict 
 import numpy as np
-import itertools
 
-Class Scorecard:
+class Scorecard:
+
+
     def __init__(self):
         self.points = 0
         labels =[
@@ -21,33 +22,60 @@ Class Scorecard:
             'yahtzee'
         ]
         scoring_functions = [
-            self.score_face(face = 1),
-            self.score_face(face = 2),
-            self.score_face(face = 3),
-            self.score_face(face = 4),
-            self.score_face(face = 5),
-            self.score_face(face = 6),
-            self.score_n_of_a_kind(n = 2),
-            self.score_n_of_a_kind(n = 3),
-            self.score_n_of_a_kind((n = 4)),
-            self.score_full_house(),
-            self.score_small_straight(),
-            self.score_large_straight(),
-            self.score_yahtzee()
+            self.score_ones,
+            self.score_twos,
+            self.score_threes,
+            self.score_fours,
+            self.score_fives,
+            self.score_sixes,
+            self.score_two_of_a_kind,
+            self.score_three_of_a_kind,
+            self.score_four_of_a_kind,
+            self.score_full_house,
+            self.score_small_straight,
+            self.score_large_straight,
+            self.score_yahtzee,
         ]
         self.categories = [Category(label, scoring_function) for label, scoring_function in zip(labels, scoring_functions)]
+
+    def score_ones(self,rolls):
+        return self.score_face(rolls, face=1)
+
+    def score_twos(self,rolls):
+        return self.score_face(rolls, face=2)
+
+    def score_threes(self,rolls):
+        return self.score_face(rolls, face=3)
+
+    def score_fours(self,rolls):
+        return self.score_face(rolls, face=4)
+
+    def score_fives(self,rolls):
+        return self.score_face(rolls, face=5)
+
+    def score_sixes(self,rolls):
+        return self.score_face(rolls, face=6)
+
     def score_face(self, rolls, face):
-        score = 0
-        bins = np.binscount(rolls)
-        if len(bins) < face:
+        bins = np.bincount(rolls)
+        if len(bins) <= face:
             return 0
         else:
             return bins[face] * face
 
+    def score_two_of_a_kind(self, rolls):
+        return self.score_n_of_a_kind(rolls, n = 2)
+    
+    def score_three_of_a_kind(self, rolls):
+        return self.score_n_of_a_kind(rolls, n = 3)
+    
+    def score_four_of_a_kind(self, rolls):
+        return self.score_n_of_a_kind(rolls, n = 4)
+    
     def score_n_of_a_kind(self, rolls, n):
         last_roll = 7
         rolls_in_a_row = 0
-        bins = np.binscount(rolls)
+        bins = np.bincount(rolls)
         last_roll = len(bins)
         for bin in bins[::-1]:
             if bin >= n:
@@ -73,34 +101,22 @@ Class Scorecard:
         return 0 
 
     def score_yahtzee(self, rolls):
-        bins = np.binscount(rolls)
+        bins = np.bincount(rolls)
         if np.max(bins) == 5:
             return 50
         return 0
 
 class Category():
-    def __init__(self, name, scoring_function)
+    def __init__(self, name, scoring_function):
         self.name = name
         self.scoring_function = scoring_function
         self.filled = False
         self.score = 0
 
-    def score(self, *args, **kwargs)
-        return self.scoring_function(*args, **kwargs)
+    def score_roll(self, rolls):
+        return self.scoring_function(rolls)
 
-    def fill(self, score)
+    def fill(self, score):
         self.filled = True
         self.score = score
-
-if __name__ == "__main__":
-    dice_values = [1,2,3,4,5,6]
-    input_sets = [p for n in range(6) for p in itertools.combinations_with_replacement(dice_values, n)]
-    output_sets = [p for p in itertools.combinations_with_replacement(dice_values, 5)]
-    print len(output_sets)
-    print len(input_sets)
-    fpd, rpd = dice_probability_dict(input_sets, output_sets)
-    print len(fpd)
-    print input_sets[11]
-    print output_sets[69]
-    print fpd[input_sets[11]][output_sets[69]]
 
