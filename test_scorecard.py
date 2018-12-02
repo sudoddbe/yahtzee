b062aa4a2a813ba8751539a66c08d1bf5a4529e0
David Belfrage <david.belfrage@hotmail.com>
2018-12-02 10:44:15 +0100

    * Added simple scoring test

--- test_scorecard.py
+++ test_scorecard.py
@@ -1,5 +1,22 @@
 from yahtzee_probability import dice_probability_dict 
-from yahtzee_scorecard import scorecard 
+from yahtzee_scorecard import Scorecard 
 import numpy as np
 
+def test_create_scorecard():
+    scorecard = Scorecard()
+    assert(scorecard.categories)
+
+def test_scoring():
+    scorecard = Scorecard()
+    print [ cat.name for cat in scorecard.categories]
+    rolls = (1,2,3,4,5)
+    for cat in scorecard.categories:
+        cat.fill(cat.score_roll(rolls))
+    for cat in scorecard.categories:
+        assert cat.filled
+        print cat.score
+    
+    
 if __name__ == "__main__":
+    test_create_scorecard()
+    test_scoring()
