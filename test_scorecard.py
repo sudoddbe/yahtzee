49a6914e7858773e67af29da75dd2d7b528ed11a
David Belfrage <david.belfrage@hotmail.com>
2018-12-02 12:14:29 +0100

    * Added more test cases

--- test_scorecard.py
+++ test_scorecard.py
@@ -6,17 +6,143 @@ def test_create_scorecard():
     scorecard = Scorecard()
     assert(scorecard.categories)
 
-def test_scoring():
+def test_all_scoring():
+    scorecard = Scorecard()
+    names = [cat.name for cat in scorecard.categories]
+
+    roll = (1,1,1,1,1)
+    expected_scores = { name:0 for name in names}
+    expected_scores['ones'] = 5
+    expected_scores['pair'] = 2
+    expected_scores['three_of_a_kind'] = 3
+    expected_scores['four_of_a_kind'] = 4
+    expected_scores['full_house'] = 5
+    expected_scores['yahtzee'] = 50
+    test_scoring(roll, expected_scores)
+
+    roll = (2,2,2,2,2)
+    expected_scores = { name:0 for name in names}
+    expected_scores['twos'] = 2*5
+    expected_scores['pair'] = 2*2
+    expected_scores['three_of_a_kind'] = 2*3
+    expected_scores['four_of_a_kind'] = 2*4
+    expected_scores['full_house'] = 2*5
+    expected_scores['yahtzee'] = 50
+    test_scoring(roll, expected_scores)
+
+    roll = (3,3,3,3,3)
+    expected_scores = { name:0 for name in names}
+    expected_scores['threes'] = 3*5
+    expected_scores['pair'] = 3*2
+    expected_scores['three_of_a_kind'] = 3*3
+    expected_scores['four_of_a_kind'] = 3*4
+    expected_scores['full_house'] = 3*5
+    expected_scores['yahtzee'] = 50
+    test_scoring(roll, expected_scores)
+
+    roll = (4,4,4,4,4)
+    expected_scores = { name:0 for name in names}
+    expected_scores['fours'] = 4*5
+    expected_scores['pair'] = 4*2
+    expected_scores['three_of_a_kind'] = 4*3
+    expected_scores['four_of_a_kind'] = 4*4
+    expected_scores['full_house'] = 4*5
+    expected_scores['yahtzee'] = 50
+    test_scoring(roll, expected_scores)
+
+    roll = (5,5,5,5,5)
+    expected_scores = { name:0 for name in names}
+    expected_scores['fives'] = 5*5
+    expected_scores['pair'] = 5*2
+    expected_scores['three_of_a_kind'] = 5*3
+    expected_scores['four_of_a_kind'] = 5*4
+    expected_scores['full_house'] = 5*5
+    expected_scores['yahtzee'] = 50
+    test_scoring(roll, expected_scores)
+
+    roll = (6,6,6,6,6)
+    expected_scores = { name:0 for name in names}
+    expected_scores['sixes'] = 6*5
+    expected_scores['pair'] = 6*2
+    expected_scores['three_of_a_kind'] = 6*3
+    expected_scores['four_of_a_kind'] = 6*4
+    expected_scores['full_house'] = 6*5
+    expected_scores['yahtzee'] = 50
+    test_scoring(roll, expected_scores)
+
+    roll = (1,2,5,5,6)
+    expected_scores = { name:0 for name in names}
+    expected_scores['ones'] = 1 
+    expected_scores['twos'] = 2 
+    expected_scores['fives'] = 2*5 
+    expected_scores['sixes'] = 6 
+    expected_scores['pair'] = 2*5 
+    test_scoring(roll, expected_scores)
+
+    roll = (1,5,5,5,6)
+    expected_scores = { name:0 for name in names}
+    expected_scores['ones'] = 1 
+    expected_scores['fives'] = 3*5 
+    expected_scores['sixes'] = 6 
+    expected_scores['pair'] = 2*5 
+    expected_scores['three_of_a_kind'] = 3*5 
+    test_scoring(roll, expected_scores)
+
+    roll = (1,5,5,5,5)
+    expected_scores = { name:0 for name in names}
+    expected_scores['ones'] = 1 
+    expected_scores['fives'] = 4*5 
+    expected_scores['pair'] = 2*5 
+    expected_scores['three_of_a_kind'] = 3*5 
+    expected_scores['four_of_a_kind'] = 4*5 
+    test_scoring(roll, expected_scores)
+
+    roll = (5,5,5,6,6)
+    expected_scores = { name:0 for name in names}
+    expected_scores['fives'] = 3*5 
+    expected_scores['sixes'] = 2*6 
+    expected_scores['pair'] = 2*6 
+    expected_scores['three_of_a_kind'] = 3*5 
+    expected_scores['full_house'] = np.sum(roll)
+    test_scoring(roll, expected_scores)
+
+    roll = (1,2,3,4,5)
+    expected_scores = { name:0 for name in names}
+    expected_scores['ones'] = 1 
+    expected_scores['twos'] = 2 
+    expected_scores['threes'] = 3 
+    expected_scores['fours'] = 4 
+    expected_scores['fives'] = 5 
+    expected_scores['small_straight'] = np.sum(roll)
+    test_scoring(roll, expected_scores)
+
+    roll = (2,3,4,5,6)
+    expected_scores = { name:0 for name in names}
+    expected_scores['twos'] = 2 
+    expected_scores['threes'] = 3 
+    expected_scores['fours'] = 4 
+    expected_scores['fives'] = 5 
+    expected_scores['sixes'] = 6 
+    expected_scores['large_straight'] = np.sum(roll)
+    test_scoring(roll, expected_scores)
+
+
+def test_scoring(roll, expected_scores):
     scorecard = Scorecard()
     print [ cat.name for cat in scorecard.categories]
-    rolls = (1,2,3,4,5)
     for cat in scorecard.categories:
-        cat.fill(cat.score_roll(rolls))
+        cat.fill(cat.score_roll(roll))
     for cat in scorecard.categories:
-        assert cat.filled
-        print cat.score
+        print "Testing scoring of %s" % cat.name
+        print "Roll : %s" % str(roll)
+        if not cat.filled:
+            print "Not filled" 
+            assert False
+        if cat.score != expected_scores[cat.name]:
+            print "Wrong score expected %i but got %i" % (expected_scores[cat.name], cat.score)
+            assert False
     
     
 if __name__ == "__main__":
     test_create_scorecard()
-    test_scoring()
+    test_all_scoring()
