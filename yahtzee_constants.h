f7390cb4bfdb321e0e08feb5b7b9bf96f3b7b30d
David Belfrage <david.belfrage@hotmail.com>
2019-06-23 14:22:33 +0200

    * Move constants to separate header

---
+++ yahtzee_constants.h
@@ -0,0 +1,12 @@
+#ifndef YAHTZEE_CONSTANTS_H_
+#define YAHTZEE_CONSTANTS_H_
+
+#define NBR_DICES (5)
+#define NBR_FACES (6)
+#define NBR_CATEGORIES (15)
+#if 0
+#define DEBUG(x) (x)
+#else
+#define DEBUG(x)
+#endif
+#endif /* YAHTZEE_CONSTANTS_H_ */
