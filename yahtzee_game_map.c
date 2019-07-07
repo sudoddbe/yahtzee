ef516219cd941c7090de1897fba8943eb3d49a34
David Belfrage <david.belfrage@hotmail.com>
2019-07-07 23:25:21 +0200

    * Main implementation completed. Will have to think about index ordering and or multithreading to increase performance

--- yahtzee_game_map.c
+++ yahtzee_game_map.c
@@ -3,32 +3,25 @@
 #include "yahtzee_scorecard.h"
 #include "yahtzee_constants.h"
 
-struct game_map* yahtzee_game_map_create()
-{
-    struct game_map* gm= calloc(1, sizeof(*gm));
-    gm->prob_mat = prob_mat_create();
-    gm->score_map = calloc(NBR_SCORECARDS * gm->prob_mat->cols * NBR_REROLLS * MAX_UPPER_SCORE, sizeof(*(gm->score_map)));
-    gm->category_score_map = calloc(NBR_CATEGORIES * gm->prob_mat->cols, sizeof(*(gm->category_score_map)));
-    precalculate_scores(gm);
-    return gm;
-}
-
-void yahtzee_game_map_destroy(struct game_map* gm)
-{
-    prob_mat_destroy(gm->prob_mat);
-    free(gm->score_map);
-    free(gm->category_score_map);
-}
-
 #define NBR_ROLLS (252)
 //Shameless temporary define
 int get_index(int scorecard_index, int roll_index, int reroll_index, int upper_score_index)
 {
+#if 1
+    assert(scorecard_index < NBR_SCORECARDS);
+    assert(roll_index < NBR_ROLLS);
+    assert(reroll_index < NBR_REROLLS);
+    assert(upper_score_index < MAX_UPPER_SCORE);
+    assert(scorecard_index > -1);
+    assert(roll_index > -1);
+    assert(reroll_index > -1);
+    assert(upper_score_index > -1);
+#endif
     int index = 0;
-    index +=  scorecard_index * (NBR_ROLLS*NBR_REROLLS*MAX_UPPER_SCORE);
-    index +=  roll_index * (NBR_REROLLS*MAX_UPPER_SCORE);
-    index +=  reroll_index * (MAX_UPPER_SCORE);
-    index +=  upper_score_index;
+    index += reroll_index * (MAX_UPPER_SCORE*NBR_SCORECARDS*NBR_ROLLS);
+    index += upper_score_index * (NBR_SCORECARDS*NBR_ROLLS);
+    index +=  scorecard_index * (NBR_ROLLS);
+    index +=  roll_index;
     return index;
 }
 
@@ -36,6 +29,7 @@ int get_index(int scorecard_index, int roll_index, int reroll_index, int upper_s
 int find_first_bit_set(int a)
 {
     int mask = 1;
+    int tmp = a;
     int counter = 0;
     while(a >0){
         if( (a & mask) == 1){
@@ -44,6 +38,7 @@ int find_first_bit_set(int a)
         a = a >> 1;
         counter++;
     }
+    assert(false);
 }
 
 int get_precalculated_score(struct game_map* gm, int category_index, int roll_index){
@@ -61,6 +56,106 @@ int precalculate_scores(struct game_map* gm)
     }
 }
 
+float scorecard_max_expected_value(struct game_map* gm, int scorecard, int us_index, int roll_index, int subturn)
+{
+    float max_value = 0;
+    int forward_scorecards[NBR_CATEGORIES] = {0};
+    unsigned int nbr_forward_scorecards = 0;
+    find_forward_scorecard(scorecard, forward_scorecards, &nbr_forward_scorecards);
+    int sc_index;
+    int prob_col;
+    struct roll* rolls = gm->prob_mat->rolls;
+    for(sc_index = 0; sc_index < nbr_forward_scorecards; sc_index++){
+        int fwd_scorecard = forward_scorecards[sc_index];
+        int category_index = find_first_bit_set(fwd_scorecard - scorecard);
+        int score = get_precalculated_score(gm, category_index, roll_index);
+        int added_upper_score = (category_index < 6) * score;
+        int tmp_upper_score = us_index + added_upper_score;
+        if(tmp_upper_score > MAX_UPPER_SCORE -1){
+            tmp_upper_score = MAX_UPPER_SCORE - 1;
+        }
+        float expected_value = 0;
+        for(prob_col = 0; prob_col<gm->prob_mat->cols; prob_col++){
+            int index = get_index(fwd_scorecard, prob_col, 0, tmp_upper_score);
+            expected_value += rolls[(gm->prob_mat->rows - 1)*gm->prob_mat->cols + prob_col].probability * gm->score_map[index];
+        }
+        expected_value += score;
+        if (expected_value > max_value){
+            max_value = expected_value;
+        }
+    }
+    return max_value;
+}
+
+void fill_select_subturn(struct game_map* gm, int turn, int* scorecards, unsigned int nbr_scorecards)
+{
+    int reroll_index = NBR_REROLLS - 1;
+    int sc_index;
+    int roll_index;
+    int us_index;
+    for( sc_index = 0; sc_index < nbr_scorecards; sc_index++){
+        int scorecard = scorecards[sc_index];
+        for( roll_index = 0; roll_index < gm->prob_mat->cols; roll_index++){
+            for( us_index = 0; us_index < MAX_UPPER_SCORE; us_index++){
+                int index = get_index(scorecard, roll_index, reroll_index, us_index);
+                gm->score_map[index] = scorecard_max_expected_value(gm, scorecard, us_index, roll_index, reroll_index);
+            }
+        }
+    }
+}
+
+float roll_max_expected_value(struct game_map* gm, int sc_index, int us_index, int roll_index, int subturn)
+{
+    int prob_row;
+    int prob_col;
+    float max_value = 0;
+    struct roll* rolls = gm->prob_mat->rolls;
+    for(prob_row =0; prob_row<gm->prob_mat->rows; prob_row++){
+        if(rolls[prob_row*gm->prob_mat->cols + roll_index].probability <= 0){
+            continue;
+        }
+        float expected_value = 0;
+        for(prob_col =0; prob_col<gm->prob_mat->cols; prob_col++){
+            int index = get_index(sc_index, prob_col, subturn + 1, us_index);
+            expected_value += rolls[prob_row*gm->prob_mat->cols + prob_col].probability * gm->score_map[index];
+        }
+        if(expected_value > max_value){
+            max_value = expected_value;
+        }
+    }
+    return max_value;
+}
+
+void fill_roll_subturn(struct game_map* gm, int turn, int reroll_index, int* scorecards, unsigned int nbr_scorecards)
+{
+    int sc_index;
+    int roll_index;
+    int us_index;
+    for( sc_index = 0; sc_index < nbr_scorecards; sc_index++){
+        int scorecard = scorecards[sc_index];
+        for( roll_index = 0; roll_index < gm->prob_mat->cols; roll_index++){
+            for( us_index = 0; us_index < MAX_UPPER_SCORE; us_index++){
+                int index = get_index(scorecard, roll_index, reroll_index, us_index);
+                gm->score_map[index] = roll_max_expected_value(gm, scorecard, us_index, roll_index, reroll_index);
+            }
+        }
+    }
+}
+
+void fill_turn(struct game_map* gm, int turn)
+{
+    int scorecards[NBR_SCORECARDS] = {0};
+    unsigned int nbr_scorecards = 0;
+    find_scorecards_for_turn(turn, scorecards, &nbr_scorecards);
+    fill_select_subturn(gm, turn, scorecards, nbr_scorecards);
+    int subturn = NBR_REROLLS - 2;
+    while (subturn >= 0){
+        printf(" Calculating : %i --- %i \n", turn, subturn);
+        fill_roll_subturn(gm, turn, subturn, scorecards, nbr_scorecards);
+        subturn--;
+    }
+}
+
 //Last subturn is when we have filled all but one category on the scorecard
 //We have only one option to fill, and will get a score based on that.
 void fill_end_turn(struct game_map* gm)
@@ -71,7 +166,7 @@ void fill_end_turn(struct game_map* gm)
     int reroll_index = NBR_REROLLS -1;
 
     int scorecards[NBR_SCORECARDS] = {0};
-    int nbr_scorecards = 0;
+    unsigned int nbr_scorecards = 0;
     find_scorecards_for_turn(NBR_CATEGORIES-1, scorecards, &nbr_scorecards);
     assert(nbr_scorecards == NBR_CATEGORIES);
 
@@ -85,7 +180,8 @@ void fill_end_turn(struct game_map* gm)
                 category_int -= scorecard;
                 int category_index = find_first_bit_set(category_int);
                 int score = get_precalculated_score(gm, category_index, roll_index);
-                if( us_index + score >= MAX_UPPER_SCORE -1){
+                int added_upper_score = (category_index < 6) * score;
+                if( us_index + added_upper_score >= MAX_UPPER_SCORE -1){
                     score += 50;
                 }
                 int index = get_index(scorecard, roll_index, reroll_index, us_index);
@@ -93,6 +189,29 @@ void fill_end_turn(struct game_map* gm)
             }
         }
     }
+    
+    reroll_index = NBR_REROLLS - 2;
+    while (reroll_index >= 0){
+        fill_roll_subturn(gm, NBR_CATEGORIES -1, reroll_index, scorecards, nbr_scorecards);
+        reroll_index--;
+    }
+}
+
+struct game_map* yahtzee_game_map_create()
+{
+    struct game_map* gm= calloc(1, sizeof(*gm));
+    gm->prob_mat = prob_mat_create();
+    gm->score_map = calloc(NBR_SCORECARDS * gm->prob_mat->cols * NBR_REROLLS * MAX_UPPER_SCORE, sizeof(*(gm->score_map)));
+    gm->category_score_map = calloc(NBR_CATEGORIES * gm->prob_mat->cols, sizeof(*(gm->category_score_map)));
+    precalculate_scores(gm);
+    return gm;
+}
+
+void yahtzee_game_map_destroy(struct game_map* gm)
+{
+    prob_mat_destroy(gm->prob_mat);
+    free(gm->score_map);
+    free(gm->category_score_map);
 }
 
 int main(void)
@@ -100,5 +219,20 @@ int main(void)
     struct game_map* gm = yahtzee_game_map_create();
     printf("%u \n", gm->prob_mat->cols);
     printf("Size of map : %ld \n", NBR_SCORECARDS * gm->prob_mat->cols * NBR_REROLLS * MAX_UPPER_SCORE);
+    fill_end_turn(gm);
+    int turn = NBR_CATEGORIES -2;
+    while(turn >= 0){
+        fill_turn(gm, turn);
+        turn--;
+    }
+    int prob_col;
+    float expected_value = 0;
+    for(prob_col = 0; prob_col<gm->prob_mat->cols; prob_col++){
+        int index = get_index(0, prob_col, 0, 0);
+        expected_value += gm->prob_mat->rolls[(gm->prob_mat->rows - 1)*gm->prob_mat->cols + prob_col].probability * gm->score_map[index];
+        struct roll tmp = gm->prob_mat->rolls[(gm->prob_mat->rows - 1)*gm->prob_mat->cols + prob_col];
+        printf(" Roll : %u, %u, %u, %u, %u, \t score : %4.8f \n", tmp.output_set[0], tmp.output_set[1], tmp.output_set[2], tmp.output_set[3], tmp.output_set[4], gm->score_map[index]);
+    }
+    printf("Expected value : %8.6f\n", expected_value);
     yahtzee_game_map_destroy(gm);
 }
